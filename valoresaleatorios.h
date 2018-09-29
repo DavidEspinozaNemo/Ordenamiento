9f47a4a4e625fe26587abcd9dfe9f04c4f2c920f
DavidEspinozaNemo <david1998es16@gmail.com>
2018-09-29 17:48:43 -0600

    Create valoresaleatorios.h

---
+++ valoresaleatorios.h
@@ -0,0 +1,54 @@
+#ifndef VALORESALEATORIOS_H
+#define VALORESALEATORIOS_H
+
+#include "listadoble.h"
+#include "lectorarchivo.h"
+#include<stdlib.h>
+#include<time.h>
+#include <stdio.h>
+#include <cstdlib>
+
+using namespace std;
+
+
+//Generar una lista de numeros aleatorios.
+listaDoble<int>* generarNumeros(int rango){
+    int num;
+    srand(time(NULL));
+    listaDoble<int> *lista = new listaDoble<int>();
+    for(int i = 0; i<rango; i++){
+        //variable = limite_inferior + rand() % (limite_superior +1 - limite_inferior) ;
+        num = 1 + rand()%1000000;
+        lista->insertarPos(num,0);
+    }
+    return lista;
+}
+//Generar una lista de letras aleatorias.
+listaDoble<char>* generarChars(int rango){
+    int num;
+    char letr;
+    srand(time(NULL));
+    listaDoble<char> *lista = new listaDoble<char>();
+    for(int i = 0; i<rango; i++){
+        //variable = limite_inferior + rand() % (limite_superior +1 - limite_inferior) ;
+        num = rand()% 97+25;
+        letr = (char)num;
+        if(letr != '}' && letr != '{' && letr != ']' && letr != '[')lista->insertarPos(letr,0);
+        else i--;
+    }
+    return lista;
+}
+
+//Generar una lista de palabras aleatorias.
+listaDoble<string>* generarPalabras(int rango){
+    listaDoble<string> *biblioteca = leerPalabras();
+    int rangoDeBibliot =biblioteca->cantDatos(), num;
+    listaDoble<string> *lista = new listaDoble<string>();
+    for(int i = 0; i<rango; i++){
+        num = rand()%(rangoDeBibliot-1);
+        lista->insertarPos(biblioteca->getDato(num),0);
+    }
+    return lista;
+}
+
+#endif // VALORESALEATORIOS_H
